1bf57bf3a26d424f4c0f24c274c072e43500e26c
marceauwr <marceau.wang-ribour@hetic.net>
2017-12-20 14:55:32 +0100

    adding-Earth-to-Layout

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-const $nextButton = document.querySelector('.next-button')
-const $previousButton = document.querySelector('.previous-button')
 const $content = document.querySelector('.content')
 let changeSlide = 0
 
@@ -12,30 +10,6 @@ const $text3 = document.querySelector('.text3')
 const $text4 = document.querySelector('.text4')
 const $text5 = document.querySelector('.text5')
 
-$nextButton.addEventListener('mousedown', () =>
-{
-    changeSlide += $content.offsetWidth
-    scroll(changeSlide, 0)
-
-    if(changeSlide > $content.offsetWidth * 4)
-    {
-        changeSlide = $content.offsetWidth * 4
-    }
-})
-
-$previousButton.addEventListener('mousedown', () =>
-{
-    changeSlide -= $content.offsetWidth
-    scroll(changeSlide, 0)
-
-    if(changeSlide < 0)
-    {
-        changeSlide = 0
-    }
-})
-
-
-
 document.addEventListener('mousewheel', (event) =>
 {
     event.preventDefault()
@@ -47,7 +21,7 @@ document.addEventListener('mousewheel', (event) =>
         //TRANSITIONS
         $page.classList.toggle('transition')
 
-        // changing slides
+        // changing slides in function of the transition
         if(changeSlide == $content.offsetWidth * 4 && event.deltaY > 0)
         {        }
         else
@@ -56,7 +30,11 @@ document.addEventListener('mousewheel', (event) =>
             if(event.deltaY > 0)
             {
                 changeSlide += $content.offsetWidth
-                scroll(changeSlide, 0)
+                setTimeout(() =>
+                {
+                    scroll(changeSlide, 0)
+                }, 500)
+
                 if(changeSlide > $content.offsetWidth * 4)
                 {
                     changeSlide = $content.offsetWidth * 4
@@ -71,7 +49,11 @@ document.addEventListener('mousewheel', (event) =>
             if(event.deltaY < 0)
             {
                 changeSlide -= $content.offsetWidth
-                scroll(changeSlide, 0)
+                setTimeout(() =>
+                {
+                    scroll(changeSlide, 0)
+                }, 500)
+
                 if(changeSlide < 0)
                 {
                     changeSlide = 0
@@ -79,7 +61,7 @@ document.addEventListener('mousewheel', (event) =>
             }
         }
 
-        //Change text
+        //Change text in function of the transition
         if(changeSlide == 0)
         {
             $text1.classList.remove('not-active')
@@ -121,7 +103,7 @@ document.addEventListener('mousewheel', (event) =>
             $text5.classList.remove('not-active')
         }
 
-        //Delay for scrolling
+        //Delay for next scroll
         setTimeout(() =>
         {
             animationIsActive = false
@@ -131,54 +113,66 @@ document.addEventListener('mousewheel', (event) =>
 })
 
 
+/* Script for Earth */
 
-// $page.addEventListener('mousedown', () =>
-// {
-//     // $page.classList.toggle('transition')
-//     // $text1.classList.toggle('not-active')
-//     // $text2.classList.toggle('not-active')
-//     // $text3.classList.toggle('not-active')
-//     // $text4.classList.toggle('not-active')
-//     // $text5.classList.toggle('not-active')
+const $canvas = document.querySelector('canvas')
+const context = $canvas.getContext('2d')
 
-//     if(changeSlide == 0)
-//     {
-//         $text1.classList.remove('not-active')
-//         $text2.classList.add('not-active')
-//         $text3.classList.add('not-active')
-//         $text4.classList.add('not-active')
-//         $text5.classList.add('not-active')
-//     }
-//     else if(changeSlide == $content.offsetWidth)
-//     {
-//         $text1.classList.add('not-active')
-//         $text2.classList.remove('not-active')
-//         $text3.classList.add('not-active')
-//         $text4.classList.add('not-active')
-//         $text5.classList.add('not-active')
-//     }
-//     else if(changeSlide == $content.offsetWidth * 2)
-//     {
-//         $text1.classList.add('not-active')
-//         $text2.classList.add('not-active')
-//         $text3.classList.remove('not-active')
-//         $text4.classList.add('not-active')
-//         $text5.classList.add('not-active')
-//     }
-//     else if(changeSlide == $content.offsetWidth * 3)
-//     {
-//         $text1.classList.add('not-active')
-//         $text2.classList.add('not-active')
-//         $text3.classList.add('not-active')
-//         $text4.classList.remove('not-active')
-//         $text5.classList.add('not-active')
-//     }
-//     else if(changeSlide == $content.offsetWidth * 4)
-//     {
-//         $text1.classList.add('not-active')
-//         $text2.classList.add('not-active')
-//         $text3.classList.add('not-active')
-//         $text4.classList.add('not-active')
-//         $text5.classList.remove('not-active')
-//     }
-// })
+const stars = []
+
+const createStars = () => {
+    stars.splice(0, 100)
+    for (let i = 0; i < 100; i++) {
+        const star = {}
+        star.x = Math.random() * $canvas.width
+        star.y = Math.random() * $canvas.height
+        star.radius = Math.random() * 1.2
+        star.color = `rgba(255, 255, 255, ${Math.random()}`
+
+        stars.push(star)
+    }
+}
+
+const resize = () => {
+    $canvas.width = window.innerWidth
+    $canvas.height = window.innerHeight
+    createStars()
+}
+
+window.addEventListener('resize', resize)
+resize()
+
+const updateStars = () => {
+    for (const star of stars) {
+        star.x += 0.2
+        if (star.x > $canvas.width) {
+            star.x = 0
+        }
+    }
+}
+
+const drawStars = () => {
+    for (const star of stars) {
+        context.beginPath()
+        context.arc(star.x, star.y, star.radius, 0, Math.PI * 2)
+        context.fillStyle = star.color
+        context.fill()
+    }
+}
+
+const clear = () => {
+    context.clearRect(0, 0, $canvas.width, $canvas.height)
+}
+
+const loop = () => {
+    requestAnimationFrame(loop)
+
+    updateStars()
+    clear()
+    drawStars()
+}
+
+loop()
+createStars()
+
+/* End script JS */
